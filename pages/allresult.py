2083f049fba28ee7c55e06b94f7750be6dfbdea6
obtuse-triangle <me@obtuse.kr>
2024-07-15 12:19:28 +0900

    add graph color

--- pages/allresult.py
+++ pages/allresult.py
@@ -26,16 +26,19 @@ st.write(df)
 
 import matplotlib.pyplot as plt
 
-# question = [0, 0, 0]
-# for i in range(len(df)):
-#     question[0] += df["question"][i][0]
-#     question[1] += df["question"][i][1]
-#     question[2] += df["question"][i][2]
+question = [0, 0, 0]
+for i in range(len(df)):
+    question[0] += df["question"][i][0]
+    question[1] += df["question"][i][1]
+    question[2] += df["question"][i][2]
 
-# plt.bar(["q1", "q2", "q3"], question)
-# plt.title("score by question")
+plt.figure().patch.set_facecolor("dimgray")
+plt.gca().set_facecolor("dimgray")
+plt.bar(["q1", "q2", "q3"], question, color="darkgrey")
+"## 문제별 합산점수"
+st.pyplot(plt)
 
-# st.pyplot(plt)
+plt.close()
 
 # 문제별 평균 with matplotlib
 avg = [0, 0, 0]
@@ -45,7 +48,28 @@ for i in range(len(df)):
     avg[2] += df["question"][i][2]
 
 avg = [i / len(df) for i in avg]
-plt.bar(["q1", "q2", "q3"], avg)
-plt.title("avg score by question")
+plt.figure().patch.set_facecolor("dimgray")
+plt.gca().set_facecolor("dimgray")
+plt.bar(["q1", "q2", "q3"], avg, color="darkgrey")
+"## 문제별 평균점수"
 
+st.pyplot(plt)
+
+plt.close()
+
+# 문제별 표준편차 with matplotlib
+import numpy as np
+
+"## 문제별 표준편차"
+
+std = [0, 0, 0]
+for i in range(len(df)):
+    std[0] += (df["question"][i][0] - avg[0]) ** 2
+    std[1] += (df["question"][i][1] - avg[1]) ** 2
+    std[2] += (df["question"][i][2] - avg[2]) ** 2
+
+std = [np.sqrt(i / len(df)) for i in std]
+plt.figure().patch.set_facecolor("dimgray")
+plt.gca().set_facecolor("dimgray")
+plt.bar(["q1", "q2", "q3"], std, color="darkgrey")
 st.pyplot(plt)
